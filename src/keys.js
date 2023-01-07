45a39095cfc608ee6f2bb90d3d3cf62ecf7d253b
Oscar jeremias jc <96687872+oscarjeremias@users.noreply.github.com>
2023-01-07 14:51:33 +0100

    Update temporadas 1

--- src/keys.js
+++ src/keys.js
@@ -1,22 +1,22 @@
 export const keys = [
   {
     key: "be871a40-7b3a-11ec-b215-9716085a1a38",
-    temporadas: ["1959", "2173"],
+    temporadas: ["3182", "3183"],
   },
   {
     key: "8ed89930-997f-11ec-a932-bbf4ec7ba973",
-    temporadas: ["2943", "1980"],
+    temporadas: ["3363", "3161"],
   },
   {
     key: "e0666270-98ac-11ec-a902-5db70a759f78",
-    temporadas: ["2029", "2022"],
+    temporadas: ["3307", "3188"],
   },
   {
     key: "2e928a80-9901-11ec-a99e-9f0b88f9b639",
-    temporadas: ["2100", "2020"],
+    temporadas: ["3236", "3194"],
   },
   {
     key: "dbf0af50-98ad-11ec-bdc5-41815e411a29",
-    temporadas: ["2063"],
+    temporadas: ["3274"],
   },
 ];
